44e731c1b0cfe4cb68e3f6ede6df5b43e4320ad5
VoidAlone <glassbox93@gmail.com>
2025-07-08 14:16:54 -0600

    adjusted dasbhoard for neovim

--- lua/plugins/utility/snacks.lua
+++ lua/plugins/utility/snacks.lua
@@ -1,3 +1,5 @@
+local header_file = vim.fn.stdpath("config") .. "/lua/config/themes/headers2.txt"
+
 return {
   "folke/snacks.nvim",
   priority = 1000,
@@ -6,30 +8,49 @@ return {
     -- your configuration comes here
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
-    bigfile = { enabled = true },
-    dashboard = {
-        sections = {
-            {
-                section = "terminal",
-                cmd = "lolcat -b ~/.config/nvim-nihilvim/lua/config/themes/headers2.txt",
-                height = 15,
-                width = 100,
-            },
-            { section = "keys", gap = 1, padding = 1 },
-            { section = "startup" },
-        },
+  bigfile = {
+    enabled = true,
+    line_length = 1500,
+  },
+  dashboard = {
+    enabled = true,
+    sections = {
+      {
+        section = "terminal",
+        -- cmd = "lolcat -b " .. vim.fn.stdpath('config') .. "/lua/config/themes/headers2.txt",
+        -- cmd = "cat " .. vim.fn.stdpath("config") .. "/lua/config/themes/dash_neovim.txt",
+        cmd = 'lolcat -f -b -g 6c17b6:ff5e00 ' .. header_file,
+        -- cmd = {
+        --     vim.o.shell, '-c',
+        --     string.format(
+        --         [[while read -r line; do 
+        --             echo "$line";
+        --             sleep 0.01;
+        --         done < "%s" | lolcat -f -b
+        --         ]],
+        --         header_file
+        --     ),
+        -- },
+        indent = -5,
+        height = 8,
+        width = 69,
+        padding = 1,
+      },
+        { section = "keys", gap = 1, padding = 1 },
+        { section = "startup" },
     },
-    explorer = { enabled = true },
-    indent = { enabled = true},
-    input = {
-          enabled = true,
-    },
-    picker = { enabled = true },
-    notifier = { enabled = true },
-    quickfile = { enabled = true },
-    scope = { enabled = true },
-    scroll = { enabled = true },
-    statuscolumn = { enabled = true },
-    words = { enabled = true },
+  },
+  explorer = { enabled = true },
+  indent = { enabled = true},
+  input = {
+    enabled = true,
+  },
+  picker = { enabled = true },
+  notifier = { enabled = true },
+  quickfile = { enabled = true },
+  scope = { enabled = true },
+  scroll = { enabled = true },
+  statuscolumn = { enabled = true },
+  words = { enabled = true },
   },
 }
