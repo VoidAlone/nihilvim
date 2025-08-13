88f0f312537e9fb85ecb03ec855c6677b15d6ffb
VoidAlone <glassbox93@gmail.com>
2025-08-13 02:56:05 -0600

    added support for godot and better roslyn config

--- lua/config/lsp/lsp-config.lua
+++ lua/config/lsp/lsp-config.lua
@@ -1,12 +1,5 @@
-vim.lsp.enable({
-    'lua_ls',
-    'ccls',
-    'pyright',
-})
-
 local home = vim.fn.expand("~")
 local target = home .. "/Repos/AltRepos/MicrosoftLanguageServer/content/LanguageServer/linux-x64"
-
 vim.lsp.config("roslyn", {
     cmd = {
         "dotnet",
@@ -15,5 +8,25 @@ vim.lsp.config("roslyn", {
         "--extensionLogDirectory=" .. vim.fs.dirname(vim.lsp.get_log_path()),
         "--stdio",
     },
+    root_dir = vim.fs.dirname(vim.fs.find({ "*.csproj", "*.sln" }, { upward = true,})[1]),
+    filetypes = {'cs'},
     -- Add other options here
+})
+
+local cmd = vim.lsp.rpc.connect('127.0.0.1', 6005)
+local pipe = '/tmp/godot.pipe'
+vim.lsp.config('godot', {
+    cmd = cmd,
+    root_dir = vim.fs.dirname(vim.fs.find({ 'project.godot', '.git' }, { upward = true })[1]),
+    filetypes = {'gdscript', 'gdshader'},
+    on_attach = function(client, bufnr)
+        vim.fn.serverstart(pipe)
+    end
+})
+
+vim.lsp.enable({
+    'lua_ls',
+    'ccls',
+    'pyright',
+    'godot',
 })
